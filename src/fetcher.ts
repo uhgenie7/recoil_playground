084dd7e68477e277a243c652524b658325aad7dd
HyejinUm <uhj1993@naver.com>
2022-08-01 16:41:39 +0900

    feat: api fetch

---
+++ src/fetcher.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import type { METHOD } from './types';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+
+const fetcher = async (method: METHOD, url: string, ...rest: { [key: string]: any }[]) => {
+  const res = await axios[method](url, ...rest);
+  return res.data;
+};
+
+export default fetcher;
