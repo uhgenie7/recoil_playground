77e113cf8e56e03eddb62e1d9a9dbc16003fe260
HyejinUm <uhj1993@naver.com>
2022-08-01 15:20:42 +0900

    Feat: API 불러보기~

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
-import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
