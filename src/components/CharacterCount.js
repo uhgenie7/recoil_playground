7aeff5bedd066ff67aecc0198cad862b807a80af
HyejinUm <uhj1993@naver.com>
2022-08-01 10:51:35 +0900

    feat: 기본적인 recoil 사용법

---
+++ src/components/CharacterCount.js
@@ -0,0 +1,10 @@
+import { charCountState } from "../states/textState";
+import { useRecoilValue } from "recoil";
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <>useRecoilValue 입니다. selector를 사용했습니다: {count}</>;
+}
+
+export default CharacterCount;
