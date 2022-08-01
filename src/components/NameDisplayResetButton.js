a35f1277e27404f2d6b1500812dcc0b21c423fd9
HyejinUm <uhj1993@naver.com>
2022-08-01 14:44:12 +0900

    Remove File

--- src/components/NameDisplayResetButton.js
+++
@@ -1,9 +0,0 @@
-import { useResetRecoilState } from "recoil";
-import { namesState } from "../states/namesState";
-
-function NameDisplayResetButton() {
-  const resetList = useResetRecoilState(namesState);
-  return <button onClick={resetList}>이름 리스트 리셋 버튼</button>;
-}
-
-export default NameDisplayResetButton;
