f68988284fc1226c5c348d61cf544d375321070f
HyejinUm <uhj1993@naver.com>
2022-08-01 16:41:31 +0900

    feat: type 정의

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export enum METHOD {
+  GET = 'get',
+  POST = 'post',
+  PUT = 'put',
+  DELETE = 'delete',
+}
+
+export interface Message {
+  id: string;
+  userId: string;
+  timestamp: number;
+  text: string;
+}
+
+export interface User {
+  id: string;
+  nickname: string;
+}
+
+export interface Users {
+  [key: string]: User;
+}
