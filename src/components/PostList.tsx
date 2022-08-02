b1aab304dce99ebb16f7a9b75af19abb8d2db371
HyejinUm <uhj1993@naver.com>
2022-08-02 17:33:34 +0900

    feat: useQuery 사용해보기 (타입까지)

---
+++ src/components/PostList.tsx
@@ -0,0 +1,35 @@
+import MsgInput from './MsgInput';
+import Post from './Post';
+import { AxiosError } from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { QueryKeys } from '@src/QueryKeys';
+import { getPosts } from '@src/api/getPosts';
+import { Posts } from '@src/types';
+
+const PostList = () => {
+  const { data, isLoading, isError, error } = useQuery<Posts[], AxiosError, Posts[], string[]>(
+    QueryKeys.POST,
+    () => getPosts,
+  );
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return console.error(error);
+  }
+
+  return (
+    <>
+      <MsgInput mutate={() => {}} />
+      <ul className="messages">
+        {data.map((x) => (
+          <Post key={x.id} {...x} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default PostList;
