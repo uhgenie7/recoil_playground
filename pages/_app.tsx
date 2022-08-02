595cca141e5736e8e09ce2cd3324a4573ab388b0
HyejinUm <uhj1993@naver.com>
2022-08-02 13:48:41 +0900

    feat: Hydrate 추가하기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
 
@@ -7,10 +7,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
-      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </Hydrate>
     </QueryClientProvider>
   );
 }
