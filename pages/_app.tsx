0f4982e4628f25d2368f1f1248d1ef3fd3bb4616
HyejinUm <uhj1993@naver.com>
2022-08-03 09:57:42 +0900

    refactor: 안정적인 쿼리 클라이언트

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,23 @@
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
 import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: 0,
-        useErrorBoundary: true,
+  const [queryClient] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: 0,
+          useErrorBoundary: true,
+        },
+        mutations: {
+          useErrorBoundary: true,
+        },
       },
-      mutations: {
-        useErrorBoundary: true,
-      },
-    },
-  });
+    }),
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
